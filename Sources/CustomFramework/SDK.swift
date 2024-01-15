17426c609267509d41bf1b2256d2ee7987c61292
Martin Tarres <martintarres@MacBook-Air-de-Martin.local>
2024-01-15 12:04:58 -0300

    Initial commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Martin Tarres on 15/01/2024.
+//
+
+import Foundation
+
+public struct SDK{
+    public static func doSomeWork(){
+        print("Doing some work")
+    }
+    
+}
