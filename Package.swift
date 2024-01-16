fb6cdd2b28ba9e4063bb80f853fc1bdb3ae52bab
Martin Tarres <martintarres@MacBook-Air-de-Martin.local>
2024-01-16 10:24:20 -0300

    version 0.0.4

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CustomFramework",
     platforms: [
-      .macOS(.v12), .iOS(.v15)
+      .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
